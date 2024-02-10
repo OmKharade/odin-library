d9f855ccaf410238190e229a7cb54e1b54dd2b38
OmKharade <133861833+OmKharade@users.noreply.github.com>
2024-02-09 22:28:31 -0500

    add index.html and book.js

---
+++ book.js
@@ -0,0 +1,57 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+function addBookToLibrary() {
+  let title = document.getElementById('title').value
+  let author = document.getElementById('author').value
+  let pages = document.getElementById('pages').value
+  let read = document.getElementById('read').checked
+  let book = new Book(title, author, pages, read)
+  myLibrary.push(book)
+  displayBooks()
+}
+
+function displayBooks(){
+    let bookList = document.querySelector(".bookList")
+    bookList.innerHTML = ""
+    for(let i = 0; i<myLibrary.length; i++) {
+        let book = myLibrary[i]
+        let bookDiv = document.createElement("div")
+        bookDiv.setAttribute("class","book-card")
+        bookDiv.innerHTML = `
+        <div class="header">
+            <h3 class="card-title">${book.title}</h3>
+        </div>
+        <div class="body">
+            <p class="card-author">by ${book.author}</p>
+            <p class="card-pages">${book.pages} pages</p>
+            <p class="card-read">${book.read? "Completed":"Not Completed"}</p>
+            <button class="card-delete" onclick="deleteBook(${i})">Delete</button>
+    
+        </div>`
+        bookList.appendChild(bookDiv)
+    };
+}
+
+function deleteBook(i){
+    myLibrary.splice(i,1)
+    displayBooks()
+}
+
+let newBookBtn = document.querySelector("#newBookBtn")
+let newBookForm = document.querySelector("#newBookForm")
+
+newBookBtn.addEventListener("click",function(){
+    newBookForm.style.display = "block"
+})
+
+newBookForm.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    addBookToLibrary()
+})
